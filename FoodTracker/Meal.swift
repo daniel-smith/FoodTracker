4d0f56253a5bd2ecd7e0b1db83bf4f6c8177b232
Daniel Smith <danielmsmith@gmail.com>
2017-07-16 17:48:22 +0100

    Added initial files

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,36 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Daniel Smith on 16/07/2017.
+//
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // Mark: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
